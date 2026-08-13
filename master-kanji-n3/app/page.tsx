1d3a1145fea8a94fc014391b5295bab36ede0f8f
dimas_mulyadi12 <172042891+dms901@users.noreply.github.com>
2026-08-13 11:26:00 +0000

    fff

--- master-kanji-n3/app/page.tsx
+++ master-kanji-n3/app/page.tsx
@@ -743,7 +743,7 @@ export default function Home() {
                   }}
                 >
 
-                  <div className="text-[clamp(100px,28vw,180px)] leading-none font-medium select-none">
+                  <div className="text-[clamp(110px,28vw,175px)] leading-none font-medium select-none">
                     {currentKanji.k}
                   </div>
 
