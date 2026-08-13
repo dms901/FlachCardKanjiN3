1b40b1621d39d120848f45852545dd589b66b787
dimas_mulyadi12 <172042891+dms901@users.noreply.github.com>
2026-08-13 08:14:33 +0000

    layout

--- master-kanji-n3/app/layout.tsx
+++ master-kanji-n3/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next'
 import { Plus_Jakarta_Sans } from 'next/font/google'
 import './globals.css'
 
-const jakarta = Plus_Jakarta_Sans({ subsets: ['latin'], weight: ['400', '700', '800'] })
+const jakarta = Plus_Jakarta_Sans({ subsets: ['latin'], weight: ['400', '500', '700', '800'] })
 
 export const metadata: Metadata = { title: 'Master Kanji N3' }
 
