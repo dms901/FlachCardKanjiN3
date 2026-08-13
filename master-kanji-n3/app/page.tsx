43d6b2a7328c1c4b295691cda625907a759ead31
dimas_mulyadi12 <172042891+dms901@users.noreply.github.com>
2026-08-13 23:47:48 +0900

    jdjdjdjjdj

--- master-kanji-n3/app/page.tsx
+++ master-kanji-n3/app/page.tsx
@@ -723,7 +723,7 @@ export default function Home() {
                     </div>
                   )}
 
-                  <div className="text-[clamp(15px,25vw,115px)] leading-none font-medium select-none transition-transform duration-300 hover:scale-105">
+                  <div className="text-[clamp(10px,20vw,100px)] leading-none font-medium select-none transition-transform duration-300 hover:scale-105">
                     {currentKanji.k}
                   </div>
 
