d7b2fdb27192c6f5ee45e3dd9a61c8f1d45eca1d
dimas_mulyadi12 <172042891+dms901@users.noreply.github.com>
2026-08-14 22:06:22 +0900

    bbbg

--- master-kanji-n3/app/page.tsx
+++ master-kanji-n3/app/page.tsx
@@ -2103,7 +2103,7 @@ export default function Home() {
 
                   <div className="absolute top-6 left-6">
 
-                   <span className="text-[clamp(8px,2.5vw,10px)] font-bold tracking-widest text-gray-300">
+                   <span className="text-[clamp(5px,2.5vw,10px)] font-bold tracking-widest text-gray-300">
                      {cardIndex + 1} / {currentCards.length}
                    </span>
 
