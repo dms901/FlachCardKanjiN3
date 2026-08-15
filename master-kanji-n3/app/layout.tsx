7c77bc629e958f37071cae4dd805c860ec71773e
dimas_mulyadi12 <172042891+dms901@users.noreply.github.com>
2026-08-15 10:28:45 +0900

    cf

--- master-kanji-n3/app/layout.tsx
+++ master-kanji-n3/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next'
 import { Plus_Jakarta_Sans } from 'next/font/google'
 import './globals.css'
 
-import AuthProvider from 'app/components/AuthProvider'
+import AuthProvider from './components/AuthProvider'
 
 const jakarta = Plus_Jakarta_Sans({
   subsets: ['latin'],
