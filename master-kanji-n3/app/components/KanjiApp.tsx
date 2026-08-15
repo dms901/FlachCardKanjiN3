10acb8da9245aa510702d720f5ff2dcf220f6c69
dimas_mulyadi12 <172042891+dms901@users.noreply.github.com>
2026-08-15 01:43:04 +0000

    eee

--- master-kanji-n3/app/components/KanjiApp.tsx
+++ master-kanji-n3/app/components/KanjiApp.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { useState, useEffect, type FormEvent } from 'react'
-import { kanjiData } from '.../data/kanjiData'
+import { kanjiData } from '../data/kanjiData'
 import { useAuth } from './AuthProvider'
 
 export default function KanjiApp() {
